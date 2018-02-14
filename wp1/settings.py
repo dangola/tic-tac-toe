aae2eeab0f8c1558f6b0e4e88281102e21a521d7
Winston Troughton <wlt1994@gmail.com>
2018-02-14 15:29:03 -0500

    Removed STATICFILES_DIRS setting

--- wp1/settings.py
+++ wp1/settings.py
@@ -123,10 +123,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
