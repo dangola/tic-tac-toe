7a04ac86fbc890417f17f5e120ebae41997847ea
Winston Troughton <wlt1994@gmail.com>
2018-02-24 12:56:06 -0500

    [WP2] Added User and Game models

---
+++ ttt/fields.py
@@ -0,0 +1,28 @@
+from django.db import models
+import ast
+
+
+class ListField(models.TextField):
+    description = "Stores a python list"
+
+    def __init__(self, *args, **kwargs):
+        super(ListField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            value = []
+
+        if isinstance(value, list):
+            return value
+
+        return ast
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+
+        return unicode(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_object(obj)
+        return self.get_db_prep_value(value)
--- ttt/models.py
+++ ttt/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+from . import fields
+
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=256)
+    verified = models.BooleanField()
+
+
+class Game(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    grid = fields.ListField()
+    winner = models.CharField(max_length=1)
