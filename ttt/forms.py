5bddaa24381e5a4e8465ba7aebde5d97ccfd46d1
dangola <adangol@cs.stonybrook.edu>
2018-02-24 20:38:04 -0500

    added css to login.html

--- ttt/forms.py
+++ ttt/forms.py
@@ -5,14 +5,14 @@ class NameForm(forms.Form):
 	name = forms.CharField(label='Name', max_length=100)
 
 class SignupForm(forms.ModelForm):
-	username = forms.CharField(label='Username')
-	password = forms.CharField(label='Password')
-	email = forms.CharField(label='Email')
+	username = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'username'}))
+	password = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'password'}))
+	email = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'email address'}))
 
 	class Meta:
 		model = User
 		fields = ['username', 'password', 'email']
 
 class LoginForm(forms.Form):
-	username = forms.CharField(label='Username')
-	password = forms.CharField(label='Password')
+	username = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'username'}))
+	password = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'password'}))
