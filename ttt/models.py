1fde29ef2cb06ccb19b46a21b00f718f05fbbe03
Winston Troughton <wlt1994@gmail.com>
2018-02-25 21:49:32 -0500

    Game class has new function get_score

--- ttt/models.py
+++ ttt/models.py
@@ -22,4 +22,20 @@ class Game(models.Model):
         return self.grid
 
     def has_winner(self):
-        return self.winner != ' '
+        return self.winner != ' ' or ' ' not in self.grid
+
+    def get_score(username):
+        user = User.objects.get(username=username)
+        query_set = Game.objects.filter(user=user)
+
+        win_count = 0
+        lose_count = 0
+        tie_count = 0
+        for game in query_set:
+            if game.winner == 'X':
+                win_count += 1
+            elif game.winner == 'O':
+                lose_count += 1
+            elif game.winner == ' ' and ' ' not in game.grid:
+                tie_count += 1
+        return win_count, lose_count, tie_count
